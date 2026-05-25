5013348cae1f2d8ec23a0b289c72f5ba61ebb922
LightCreator1007 <arunitchakraborty2006@gmail.com>
2026-05-26 03:15:32 +0530

    chore: add commitlint and husky for commit message validtion

---
+++ commitlint.config.mjs
@@ -0,0 +1,31 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-max-length": [2, "always", 100],
+    "header-max-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 100],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+  },
+};
